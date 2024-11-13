29dc6c63d2fc1f545758b4a8679338747c2ec5bf
papashady <zamani.nima18@gmail.com>
2024-11-13 21:55:23 +0330

    feat: created 'categories.js' file in 'utils' folder to handle all functionalities related to categories and rendered main categories in sidebar dynamically.

--- public/JS/posts.js
+++ public/JS/posts.js
@@ -1,13 +1,17 @@
 import { getPosts, renderPosts } from "../../utils/posts.js";
 import { getFromLocalStorage, removeLoader } from "../../utils/sharedUtils.js";
+import { getAllCategories, renderMainCategoriesInSideBar } from "../../utils/categories.js";
 
 const $ = document;
 const postsWrapper = $.getElementById("postsWrapper");
 const noPostsAlertBox = $.getElementById("noPostsAlertBox");
+const sidebarCategoriesWrapper = $.getElementById("sidebarCategoriesWrapper");
 
 window.addEventListener("load", async () => {
   const selectedCities = getFromLocalStorage("cities");
   const postsResponse = await getPosts(selectedCities[0].id);
+  const categoriesResponse = await getAllCategories();
   renderPosts(postsResponse.data.posts, postsWrapper, noPostsAlertBox);
+  renderMainCategoriesInSideBar(categoriesResponse.data.categories, sidebarCategoriesWrapper);
   removeLoader();
 });
---
+++ utils/categories.js
@@ -0,0 +1,43 @@
+import { baseApiUrl } from "./constants.js";
+
+// since the server dose not provide dynamic icon for each category, this variable will do the job.
+// with the 'slug' property of each category that comes from server, we can access each property of this object.
+// I used Font-Awesome icons.
+const categoriesIcons = {
+  real_state: "fa-regular fa-city",
+  vehicle_posts: "fa-regular fa-car",
+  "electronic-devices": "fa-regular fa-mobile",
+  appliances: "fa-regular fa-lamp",
+  services: "fa-regular fa-brush",
+  personal_items: "fa-regular fa-watch",
+  hobbies: "fa-regular fa-dice",
+  social: "fa-regular fa-users",
+  equipments: "fa-regular fa-microphone",
+  hiring: "fa-regular fa-briefcase",
+};
+
+const getAllCategories = async () => {
+  const res = await fetch(`${baseApiUrl}category`);
+  return await res.json();
+};
+
+const renderMainCategoriesInSideBar = (categoriesArray, wrapperElement) => {
+  wrapperElement.innerHTML = "";
+  console.log(categoriesArray);
+  const allCategories = categoriesArray
+    .map(
+      (category) =>
+        `<li class="sidebar__category">
+            <a href="Javascript:void(0)" class="sidebar__category-link">
+                <i class="${
+                  categoriesIcons[category.slug]
+                } sidebar__category-icon"></i>
+                <span class="sidebar__category-title">${category.title}</span>
+            </a>
+        </li>`
+    )
+    .join("");
+  wrapperElement.insertAdjacentHTML("beforeend", allCategories);
+};
+
+export { getAllCategories, renderMainCategoriesInSideBar };
