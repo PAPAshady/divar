1289a82be80ce4747d7c958f56363af3aeed298d
Nima Zamani <zamani.nima18@gmail.com>
2024-12-03 03:09:03 +0330

    feat(sharedUtils.js): handled popular searches for global search input and its drop down.

--- public/JS/shared.js
+++ public/JS/shared.js
@@ -1,8 +1,14 @@
-import { setUrlParam, getUrlParam } from "../../utils/sharedUtils.js";
+import {
+  setUrlParam,
+  getUrlParam,
+  showModal,
+  hideModal,
+} from "../../utils/sharedUtils.js";
 import { getAndRenderPosts } from "../../utils/posts.js";
 
 const $ = document;
 const globalSearchInput = $.getElementById("globalSearchInput");
+const popularSearchesWrapper = $.getElementById("popularSearchesWrapper");
 
 // handle global seach input
 const searchValue = getUrlParam("search");
@@ -16,3 +22,51 @@ globalSearchInput.addEventListener("keydown", (e) => {
     getAndRenderPosts();
   }
 });
+
+globalSearchInput.addEventListener("focus", () => {
+  showModal(".header", "show-search-drop-down");
+});
+
+window.addEventListener("click", () => {
+  const globalSearchInputWrapper = document.getElementById(
+    "globalSearchInputAndPopularSearchesDropDownWrapper"
+  );
+
+  window.addEventListener("click", (e) => {
+    if (!globalSearchInputWrapper.contains(e.target)) {
+      hideModal(".header", "show-search-drop-down");
+    }
+  });
+});
+
+const popularSearchesArray = [
+  "خودرو",
+  "ساعت",
+  "موبایل",
+  "لباس",
+  "جواهرات",
+  "اکسسوری",
+];
+
+popularSearchesWrapper.innerHTML = "";
+
+const allPopularSearches = popularSearchesArray
+  .map(
+    (popularSearchTag) =>
+      `<a 
+        class="header__popular-search-tag"
+        href="Javascript:void(0)"
+        onclick="popularSearchClickHandler('${popularSearchTag}')"
+      >
+        ${popularSearchTag}
+      </a>`
+  )
+  .join("");
+
+popularSearchesWrapper.insertAdjacentHTML("beforeend", allPopularSearches);
+
+window.popularSearchClickHandler = (popularSearchTag) => {
+  setUrlParam("search", popularSearchTag);
+  getAndRenderPosts();
+  hideModal(".header", "show-search-drop-down");
+};
