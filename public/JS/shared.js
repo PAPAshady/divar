cfc5c1191491c6624a2a01e85b0098cda6b93e18
Nima Zamani <zamani.nima18@gmail.com>
2024-12-03 02:19:45 +0330

    feat(shared.js): implemented search feature for posts.

--- public/JS/shared.js
+++ public/JS/shared.js
@@ -1,16 +1,18 @@
 import { setUrlParam, getUrlParam } from "../../utils/sharedUtils.js";
+import { getAndRenderPosts } from "../../utils/posts.js";
 
 const $ = document;
-const globalSearchInput = $.getElementById('globalSearchInput')
+const globalSearchInput = $.getElementById("globalSearchInput");
 
 // handle global seach input
-const searchValue = getUrlParam('search')
-if(searchValue){
-    globalSearchInput.value = searchValue
+const searchValue = getUrlParam("search");
+if (searchValue) {
+  globalSearchInput.value = searchValue;
 }
 
-globalSearchInput.addEventListener('keydown', e => {
-    if(e.key === 'Enter' && e.target.value.trim()) {
-        setUrlParam('search', e.target.value.trim())
-    }
-})
+globalSearchInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    setUrlParam("search", e.target.value.trim());
+    getAndRenderPosts();
+  }
+});
